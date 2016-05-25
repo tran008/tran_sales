3b33bbb04ae088adc979547e1e212b1212684723
thai008 <tran@me.com>
2016-05-25 18:25:58 +0200

    Customer (amount, creditlimit, firstname, id, openingDate)

--- tran_sales/src/domain/Customer.java
+++ tran_sales/src/domain/Customer.java
@@ -1,5 +1,70 @@
 package domain;
 
-public class Customer {
+import java.time.LocalDateTime;
 
+public class Customer {
+	private Long	   		id;
+	private LocalDateTime 	openingDate;
+	private String    		firstname;
+	private Double    		amount;
+	private Double    		creditlimit;
+	
+	
+	public Customer(Long id, LocalDateTime openingDate, String firstname, Double amount, Double creditlimit) {
+		setId( id );
+		setOpeningDate(openingDate);
+		setFirstname (firstname);
+		setAmount(amount);
+		setCreditlimit (creditlimit);
+	}
+	
+	public Customer( String firstname, Double amount ) {
+		this( null, null, firstname, amount, null);
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId( Long id ) {
+		this.id = id;
+	}
+	public LocalDateTime getOpeningDate() {
+	
+		return openingDate;
+	}
+
+	public void setOpeningDate(LocalDateTime openingDate) {
+		this.openingDate = openingDate;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	public Double getCreditlimit() {
+		return creditlimit;
+	}
+	
+	public void setCreditlimit(Double creditlimit) {
+		this.creditlimit = creditlimit;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", openingDate=" + openingDate + ", firstname=" + firstname + ", amount=" + amount
+				+ ", creditlimit=" + creditlimit + "]\n";
+	}
 }
